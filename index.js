2d2a39c2e62ab0b878da8ce328b15ee42e5f4b78
CleopasMMuchiri <cleopasmmuchiri@gmail.com>
2026-06-20 12:12:11 +0300

    first push

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+function calculateTax(amount) {
+  const tax = (10 / 100) * amount;
 
+  return tax;
+}
 
+function convertToUpperCase(text) {
+  const uppercase = text.toUpperCase();
 
+  return uppercase;
+}
 
+function findMaximum(num1, num2) {
+  const max = Math.max(num1, num2);
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+  return max;
+}
+
+function isPalindrome(word) {
+  const cleanWord = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  const reversedWord = cleanWord.split("").reverse().join("");
+
+  return cleanWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountedPercentage) {
+  const discountedPrice = (discountedPercentage / 100) * originalPrice;
+
+  const total = originalPrice - discountedPrice;
+
+  return total;
+}
+
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
